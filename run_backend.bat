a437baa92bec99689816b7ef60b5ec014904bb43
mathcast-30 <mathcastel30@gmail.com>
2026-08-29 18:22:25 +0200

    Update run_backend.bat

--- run_backend.bat
+++ run_backend.bat
@@ -1,7 +1,7 @@
 @echo off 
-title OptiCut API Backend 
+title OptiCut Pro Server (Port 8000) 
 cd /d "C:\Users\Mathe\Documents\Matheo\passion\logiciel\logiciel_V6\Moteur\Backend\System\Bin" 
 echo ============================================================ 
-echo   OPTICUT PRO - API BACKEND (Port 8000) 
+echo   OPTICUT PRO - SERVEUR APPLICATION (Port 8000) 
 echo ============================================================ 
-"C:\Users\Mathe\anaconda3\envs\opticut_pro\python.exe" -m uvicorn app.main:app --reload --host 0.0.0.0 --port 8000 
+"C:\Users\Mathe\anaconda3\envs\opticut_pro\python.exe" -m uvicorn app.main:app --host 0.0.0.0 --port 8000 
